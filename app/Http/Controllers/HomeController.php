56769217ffbf4f2950ea9f9041f1adf68a430352
Zaaaaaae25 <ezafadlan509@gmail.com>
2025-08-29 17:29:35 +0700

    fsdf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
     public function index()
     {
         $products = Product::latest()->take(6)->get();
-        
+
         // GANTI BARIS INI
         // Dari: $categories = Category::pluck('name');
         // Menjadi:
